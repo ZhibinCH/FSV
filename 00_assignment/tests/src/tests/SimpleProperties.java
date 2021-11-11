8e2ae43818498e75899a18c88d8b2c9e1da94152
Cheng, Zhibin <sebastianbingo@gmail.com>
2021-11-11 08:54:16 +0100

    test non-negativity of abs

--- 00_assignment/tests/src/tests/SimpleProperties.java
+++ 00_assignment/tests/src/tests/SimpleProperties.java
@@ -3,6 +3,7 @@ package tests;
 import org.junit.jupiter.api.Assertions;
 
 import net.jqwik.api.Property;
+import net.jqwik.api.Assume;
 import net.jqwik.api.ForAll;
 
 
@@ -14,6 +15,16 @@ public class SimpleProperties {
 	void checkAbsValue(@ForAll int x) {
 		int y = Simple.abs(x);
 		Assertions.assertTrue(y == x || y == -x);
+	}
+	
+	@Property
+	void checkAbsNonNegativity(@ForAll int x) {
+		
+		// rule out Integer.MIN_VALUE
+		Assume.that(x != Integer.MIN_VALUE);
+		
+		int y = Simple.abs(x);
+		Assertions.assertTrue(y >= 0);
 	}
 	
 }
